f1cff6ea79fc4be1031a31a8b15357ac4eff657f
Marie Houser <joe.and.marie.houser@gmail.com>
2017-10-07 17:29:14 -0500

    finished part one

---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, redirect, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blogging@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+app.secret_key = 'y337kGcys&zP3B'
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    body = db.Column(db.Text)
+
+    def __init__(self, title):
+        self.title = title
+
+
+@app.route('/blog', methods=['GET'])    
+def index():
+    blog_posts = Blog.query.all()
+    return render_template('blog.html', blog_posts=blog_posts)
+
+@app.route('/newpost', methods=['POST'])
+def new_post():
+    blog_title = request.form['blog_title']
+    blog_text = request.form['blog_text']
+    if blog_title=="":
+        flash("Please title your blog post", 'error')
+        return render_template('/newpost.html', blog_text=blog_text)
+    elif blog_text=="":
+        flash("Please enter text for this blog post", 'error') 
+        return render_template('/newpost.html', blog_title=blog_title) 
+    else:
+        new_post = Blog(blog_title)
+        new_post.body=blog_text
+        db.session.add(new_post)
+        db.session.commit()
+    return redirect('/blog')
+
+@app.route('/newpost', methods=['GET'])
+def get_newpost_template():
+    return render_template('newpost.html')
+
+if __name__ == '__main__':
+    app.run()
