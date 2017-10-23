85fe5d754e03c892a7da6cdd451ac0e8f7d7fe51
Marie Houser <joe.and.marie.houser@gmail.com>
2017-10-22 21:39:51 -0500

    with hashing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template, flash, session
 from flask_sqlalchemy import SQLAlchemy
+from hashutils import make_pw_hash, check_pw_hash
 
 
 app = Flask(__name__)
@@ -24,16 +25,17 @@ class Blog(db.Model):
 class User(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     username=db.Column(db.String(255))
-    password=db.Column(db.String(255))
+    pw_hash = db.Column(db.String(120))
+    # blogs = db.relationship('Blog', backref = 'owner_id')
 
     def  __init__(self, username, password):
         self.username=username
-        self.password=password
+        self.pw_hash = make_pw_hash(password)
     
 @app.before_request
 def require_login():
     allowed_routes = ['login', 'list_blogs', 'index', 'signup', 'display_user_posts']
-    if request.endpoint not in allowed_routes and 'user' not in session:
+    if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login')
     
 
@@ -84,7 +86,7 @@ def login():
         username = request.form['username']
         password = request.form['password']
         user = User.query.filter_by(username=username).first()
-        if user and user.password == password:
+        if user and check_pw_hash(password, user.pw_hash):
             session['user'] = username
             flash("Logged in")
             return redirect ('/newpost')
